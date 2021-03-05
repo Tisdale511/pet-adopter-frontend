7e1c7d84722950293862cca1b1376d10785d6cdf
Tisdale511 <tisdalefry@gmail.com>
2021-03-05 15:54:14 -0500

    added code for nav bar

--- adopter-frontend/src/App.js
+++ adopter-frontend/src/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
+import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
-    // ...
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/pets" className="navbar-brand">
+            Pet Adopter
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/pets"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/pets"]} component={TutorialsList} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route path="/pets/:id" component={Tutorial} />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
